4e46a74ff31901d6eee3a877b7925bce0074697a
Jaco0987752 <0987752@hr.nl>
2021-04-06 12:37:01 +0200

    moving folders

--- update.sh
+++ update.sh
@@ -1,14 +1,4 @@
-sudo cp ./*.php /var/www/profpills/
-sudo cp ./*.html /var/www/profpills/
-sudo cp ./*.css /var/www/profpills/
+sudo cp -r ./client/* /var/www/profpills/
 
-mkdir /var/www/profpills/API/
-mkdir /var/www/profpills/client/
+sudo cp -r ./API/* /var/www/profpills/API
 
-sudo cp ./client/*.php /var/www/profpills/client/
-sudo cp ./client/*.html /var/www/profpills/client/
-sudo cp ./client/*.css /var/www/profpills/client/
-
-sudo cp ./API/*.php /var/www/profpills/API/
-sudo cp ./API/*.html /var/www/profpills/API/
-sudo cp ./API/*.css /var/www/profpills/API/ 
