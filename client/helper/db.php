8a03878a74c6e4788d193027b7485e4dc0fc2989
Jaco0987752 <0987752@hr.nl>
2021-04-07 13:32:56 +0200

    updating imports

--- client/helper/db.php
+++ client/helper/db.php
@@ -3,5 +3,6 @@ $con = mysqli_connect("localhost","ProfessionalPills","1234567890","Professional
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  die();
   }
 ?>
