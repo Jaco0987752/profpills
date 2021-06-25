1a0c8359c7de224a3563d7ead14d6ac9c7fa84a8
Jaco0987752 <0987752@hr.nl>
2021-06-25 21:02:15 +0200

    updating update script

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@ sudo rm -r /var/www/profpills/
 sudo mkdir /var/www/profpills/
 sudo cp -r ./*.php /var/www/profpills/
 sudo cp -r ./*.css /var/www/profpills/
+sudo mkdir /var/www/profpills/API/
 sudo cp -r ./API/* /var/www/profpills/API/
+sudo mkdir /var/www/profpills/admin/
 sudo cp -r ./admin/* /var/www/profpills/admin/
+sudo mkdir /var/www/profpills/Helper/
 sudo cp -r ./Helper/*.php /var/www/profpills/Helper/
 
 
