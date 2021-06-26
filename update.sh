0d8b1286d021aa052c08929e7de35e3c4e42b0d4
Jaco0987752 <0987752@hr.nl>
2021-06-26 14:20:10 +0200

    update script

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
-sudo rm -r /var/www/profpills/
-sudo mkdir /var/www/profpills/
-sudo cp -r ./*.php /var/www/profpills/
-sudo cp -r ./*.css /var/www/profpills/
-sudo mkdir /var/www/profpills/API/
-sudo cp -r ./API/* /var/www/profpills/API/
-sudo mkdir /var/www/profpills/admin/
-sudo cp -r ./admin/* /var/www/profpills/admin/
-sudo mkdir /var/www/profpills/Helper/
-sudo cp -r ./Helper/*.php /var/www/profpills/Helper/
+sudo rm -r /var/www/profpills
+sudo mkdir /var/www/profpills
+sudo cp -r ./*.php /var/www/profpills
+sudo cp -r ./*.css /var/www/profpills
+sudo mkdir /var/www/profpills/API
+sudo cp -r ./API/* /var/www/profpills/API
+sudo mkdir /var/www/profpills/admin
+sudo cp -r ./admin/* /var/www/profpills/admin
+sudo mkdir /var/www/profpills/Helper
+sudo cp -r ./Helper/*.php /var/www/profpills/Helper
 
 sudo mysql -u root < ./Helper/database.sql
 
